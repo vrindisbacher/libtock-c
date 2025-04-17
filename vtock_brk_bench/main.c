0a7f4cbdf2e7ce2947b4e6888b975eb5a2e14c90
vrindisbacher <vrindisbacher77@gmail.com>
2025-04-16 20:46:59 -0700

    fixup comparisons for vtock_brk_bench

--- vtock_brk_bench/main.c
+++ vtock_brk_bench/main.c
@@ -22,5 +22,34 @@ int main(void) {
     original_heap_end += 1;
     counter += 1;
   }
-  printf("Failed after %d invocations of brk", counter);
+
+
+  ret = memop(2, 0); // start
+  if (ret.status != TOCK_STATUSCODE_SUCCESS) {
+    return 1;
+  }
+  int mem_start = ret.data;
+
+  ret = memop(1, 0); // heap end
+  if (ret.status != TOCK_STATUSCODE_SUCCESS) {
+    return 1;
+  }
+  int heap_end = ret.data;
+
+  ret = memop(6, 0); // grant region begin
+  if (ret.status != TOCK_STATUSCODE_SUCCESS) {
+    return 1;
+  }
+  int grant_end = ret.data;
+
+  ret = memop(3, 0); // memory end
+  if (ret.status != TOCK_STATUSCODE_SUCCESS) {
+    return 1;
+  }
+  int mem_end = ret.data;
+
+  printf("Failed after %d invocations of brk\n", counter);
+  printf("The process accessible ram size is %d\n", heap_end - mem_start);
+  printf("The total process size is %d\n", mem_end - mem_start);
+  printf("The amount of memory unused is: %d", grant_end - heap_end);
 }
